4badfd230fbffd6765472e52b501a6e379b7e010
CarlosMena01 <cmena@unal.edu.co>
2023-03-11 10:33:56 -0500

    Download CV

--- src/homePage/index.jsx
+++ src/homePage/index.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { AppContext } from "../data/context";
+
 import profile_img from "../assets/profile_img.png"
 import pattern from "../assets/pattern.svg"
+
 import "./homePage.css"
+
+import { es_LA } from "../data/lenguages";
+
+import CV_Spanish from "../assets/CV_Spanish.pdf"
+import CV_English from "../assets/CV_English.pdf"
 
 function HomePage(){
     const {lenguage} = React.useContext(AppContext);
+    function downloadCV() {
+        if (lenguage === es_LA) {
+            window.open( CV_Spanish, "_blank");
+        } else {
+            window.open( CV_English, "_blank");
+        }
+        
+    }
     return(
         <section className="HomePage page">
             <div className="left-div">
@@ -13,7 +28,7 @@ function HomePage(){
                 <h1> Carlos Mena </h1>
                 <p className="role"> { lenguage.homePage.item2}</p>
                 <div className="buttons-container">
-                    <button className="main-button">{ lenguage.homePage.button1}</button>
+                    <button onClick={downloadCV} className="main-button">{ lenguage.homePage.button1}</button>
                     <button onClick={function(){window.location.href="#" + lenguage.navegator.item5}} className="second-button">{ lenguage.homePage.button2}</button>
                     
                 </div>
