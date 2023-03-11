0bcbeae24fb0ac73543504b3a58bc6f30b71f641
CarlosMena01 <cmena@unal.edu.co>
2023-03-10 21:30:04 -0500

    Traducción español

--- src/data/lenguages.js
+++ src/data/lenguages.js
@@ -29,24 +29,24 @@ const en_US = {
         btn3: "Data analyst assistent",
         table1:{
         data:["Engineer intern","TeCrea", "from Jul/2022 to Dec/2022"],
-        exp: ["Decgasj asss impson sdas csSupporting hardward and firmware development.", "Decgasj asss impson sdas csSupporting hardward and firmware development.", "Soy lo máximo"]
+        exp: ["Support for the electronics and IoT projects in varius sector of the industry.", "Develop firmware projects with different MCUs.", "Minimize errors and noise in circuit designs."]
         },
         table2:{
         data:["Academic monitor","UnalMed", "from Jul/2022 to Dec/2022"],
-        exp: ["Decgasj asss impson sdas csSupporting hardward and firmware development.", "Decgasj asss impson sdas csSupporting hardward and firmware development.", "Soy lo máximo"]
+        exp: ["Support for the teaching tasks in the course of fundamentals of circuits.", "Preparation laboratory practices and help in student projects.", "Develop learn strategies for the students"]
         },
         table3:{
         data:["Data analyst assistent","UnalMed", "from Jul/2022 to Dec/2022"],
-        exp: ["Decgasj asss impson sdas csSupporting hardward and firmware development.", "Decgasj asss impson sdas csSupporting hardward and firmware development.", "Soy lo máximo"]
+        exp: ["Ensure optimal collection of contamination data.", "Analyze the accuracy of machine learning models to predict the data.", "Create grapical systems to visualize the data"]
         }
     },
     educationPage:{
         title: "Education",
         course: "Courses",
         edu: [
-            ["Computer Science", "UNAL", "Supporting hardward and firmware development lorep ummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."],
-            ["Physics Engineering", "UNAL", "Supporting hardward and firmware development lorep ummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."],
-            ["Technical high school in systems", "SENA", "Supporting hardward and firmware development lorep ummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."],
+            ["Computer Science", "UNAL", "Double degree student in computer science with an interest in algorithms, web development, machine learning and cloud computing."],
+            ["Physics Engineering", "UNAL", "Physics engineering student, member of the scientific instrumentation group and opto-digital processing group."],
+            ["Technical high school in systems", "SENA", "Systems technician with emphasis on preventive and corrective maintenance of software and hardware"],
         ],
         courses: [
             ["FastAPI","Platzi"],
@@ -62,8 +62,66 @@ const en_US = {
 
 const es_LA = {
     navegator:{
-        item1: "Hola mundo"
+        item1: "Acerca de",
+        item2: "Proyectos",
+        item3: "Experiencia",
+        item4: "Educación",
+        item5: "Contacto",
+    },
+    homePage:{
+        item1: "Hola, soy",
+        item2: "Desarrollador Backend",
+        button1: "Descargar CV",
+        button2: "Contactar"
+    },
+    aboutPage:{
+        title: "Sobre mí",
+        p1: "¡Hola! Soy Carlos Mena, un estudiante altamente motivado y apasionado de la ingeniería física y la ciencia de la computación de Colombia con un gran interés en la tecnología y el deseo de aprender y desarrollar habilidades en áreas emergentes.",
+        p2: "Tengo experiencia en lenguajes de programación como Python, Java y C, y una comprensión sólida de los principios fundamentales de la física y las matemáticas.",
+        p3: "Estoy buscando oportunidades para aplicar mis conocimientos y habilidades para resolver problemas del mundo real en un entorno de trabajo colaborativo, con el objetivo de iniciar una carrera como desarrollador backend."
+    },
+    projectsPage:{
+        title: "Proyectos",
+        prj1: "Una copia de Instagram, pero solo se pueden publicar fotos de gatos, utilicé mi propia red neuronal para esto :D",
+    },
+    experiencePage:{
+        title: "Experiencia",
+        btn1: "Prácticas de ingeniería",
+        btn2: "Monitor académico",
+        btn3: "Asistente de análisis de datos",
+        table1:{
+            data:["Prácticas de ingeniería","TeCrea", "de julio/2022 a diciembre/2022"],
+            exp: ["Apoyo para proyectos electrónicos e IoT en diversos sectores de la industria.", "Desarrollo de proyectos de firmware con diferentes MCUs.", "Minimizar errores y ruido en diseños de circuitos."]
+        },
+        table2:{
+            data:["Monitor académico","UnalMed", "de julio/2022 a diciembre/2022"],
+            exp: ["Apoyo para las tareas de enseñanza en el curso de fundamentos de circuitos.", "Preparación de prácticas de laboratorio y ayuda en proyectos de estudiantes.", "Desarrollar estrategias de aprendizaje para los estudiantes."]
+        },
+        table3:{
+            data:["Asistente de análisis de datos","UnalMed", "de julio/2022 a diciembre/2022"],
+            exp: ["Asegurar la recolección óptima de datos de contaminación.", "Analizar la precisión de los modelos de aprendizaje automático para predecir los datos.", "Crear sistemas gráficos para visualizar los datos."]
+        }
+    }, 
+    educationPage:{
+        title: "Educación",
+        course: "Cursos",
+        edu: [
+        ["Ciencia de la Computación", "UNAL", "Estudiante de doble titulación en ciencia de la computación con interés en algoritmos, desarrollo web, aprendizaje automático y computación en la nube."],
+        ["Ingeniería Física", "UNAL", "Estudiante de ingeniería física, miembro del grupo de instrumentación científica y del grupo de procesamiento óptico-digital."],
+        ["Bachillerato técnico en sistemas", "SENA", "Técnico en sistemas con énfasis en el mantenimiento preventivo y correctivo de software y hardware"],
+        ],
+        courses: [
+        ["FastAPI","Platzi"],
+        ["Redes neuronales y aprendizaje profundo","Deeplearning.IA"],
+        ["Fundamentos de visión por computadora","Universidad de Buffalo"]
+        ]
+    },
+    contactPage:{
+        title: "Hablemos",
+        description: "Actualmente estoy buscando nuevas oportunidades, mi bandeja de entrada siempre está abierta. Si tienes alguna pregunta, si deseas contratarme o simplemente quieres saludar."
     }
 }
+
+        
 
 export {en_US,es_LA}
